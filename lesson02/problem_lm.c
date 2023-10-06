10d4a63a03f0a0b6c4cc7fa4b9d34cd4251443e2
hgage2003 <hgage2003@gmail.com>
2023-10-07 00:47:17 +0300

    problem_lm added

---
+++ lesson02/problem_lm.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void read_input(unsigned *n)
+{
+    int ninput = scanf("%u", n);
+    if ((ninput != 1) || (*n < 2))
+    {
+        printf("Error: wrong input\n");
+        abort();
+    }
+}
+
+unsigned gcd(unsigned long long a, unsigned long long b)
+{
+    unsigned long long q, r;
+
+	do
+	{
+		r = a % b;
+		q = a / b;
+		a = b; b = r;
+	}
+	while (r != 0);
+
+    return a;
+}
+
+int main()
+{
+    unsigned n;
+    unsigned long long res;
+    read_input(&n);
+    res = 2;
+
+    for (int i = 3; i <= n; ++i)
+    {
+        res = res * i / gcd(res, i);
+    }
+
+    printf("%llu\n", res);
+}
