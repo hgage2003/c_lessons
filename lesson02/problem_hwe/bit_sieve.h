f58c78d02ac31b36bc39fb1bbcd54e54483f4c1c
hgage2003 <hgage2003@gmail.com>
2023-10-25 00:59:41 +0300

    bit_sieve.h cleanup

--- lesson02/problem_hwe/bit_sieve.h
+++ lesson02/problem_hwe/bit_sieve.h
@@ -6,9 +6,7 @@ struct sieve_t {
   unsigned char *mod5;  
 };
 
-int is_prime(struct sieve_t *sv, unsigned long long n);
-void set_not_prime(struct sieve_t *sv, unsigned long long n);
-void fill_sieve(struct sieve_t *sv);
+unsigned long long sieve_bound(unsigned long long num);
 struct sieve_t init_sieve(unsigned long long size_bytes);
 void free_sieve(struct sieve_t *sv);
-unsigned long long sieve_bound(unsigned long long num);
+int is_prime(struct sieve_t *sv, unsigned long long n);
