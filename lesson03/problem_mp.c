dee251f53b1592175bdb6509f7f7fa69749f2b96
hgage2003 <hgage2003@gmail.com>
2023-12-12 18:48:55 +0300

    cleanup

--- lesson03/problem_mp.c
+++ lesson03/problem_mp.c
@@ -3,103 +3,103 @@
 struct poly
 {
     int size;
-    int *coefficients;
+    int *coeff;
 };
 
-void read_input(struct poly *f, struct poly *s)
+void read_input(struct poly *a, struct poly *b)
 {
-    int ninput = scanf("%d", &f->size);
+    int ninput = scanf("%d", &a->size);
     if (ninput != 1)
     {
         printf("Error: wrong input\n");
         abort();
     }
 
-    ninput = scanf("%d", &s->size);
+    ninput = scanf("%d", &b->size);
     if (ninput != 1)
     {
         printf("Error: wrong input\n");
         abort();
     }
 
-    f->coefficients = calloc(f->size, sizeof(int));
-    if (f->coefficients == NULL)
+    a->coeff = calloc(a->size, sizeof(int));
+    if (a->coeff == NULL)
     {
         printf("Error in calloc\n");
         abort();
     }
 
-    for (int i = 0; i < f->size; ++i)
+    for (int i = 0; i < a->size; ++i)
     {
-        ninput = scanf("%d", &f->coefficients[i]);
+        ninput = scanf("%d", &a->coeff[i]);
         if (ninput != 1)
         {
             printf("Error: wrong input\n");
-            free(f->coefficients);
+            free(a->coeff);
             abort();
         }
     }
 
-    s->coefficients = calloc(s->size, sizeof(int));
-    if (s->coefficients == NULL)
+    b->coeff = calloc(b->size, sizeof(int));
+    if (b->coeff == NULL)
     {
         printf("Error in calloc\n");
-        free(f->coefficients);
+        free(a->coeff);
         abort();
     }
 
-    for (int i = 0; i < s->size; ++i)
+    for (int i = 0; i < b->size; ++i)
     {
-        ninput = scanf("%d", &s->coefficients[i]);
+        ninput = scanf("%d", &b->coeff[i]);
         if (ninput != 1)
         {
             printf("Error: wrong input\n");
-            free(f->coefficients);
-            free(s->coefficients);
+            free(a->coeff);
+            free(b->coeff);
             abort();
         }
     }
 }
 
-struct poly poly_mult(const struct poly *f, const struct poly *s)
+struct poly poly_mult(const struct poly *a, const struct poly *b)
 {
     struct poly res;
-    res.size = f->size + s->size - 1;
-    res.coefficients = calloc(res.size, sizeof(int));
+    res.size = a->size + b->size - 1;
+    res.coeff = calloc(res.size, sizeof(int));
 
-    if (res.coefficients == NULL)
+    if (res.coeff == NULL)
     {
         printf("Error in calloc\n");
         return res;
     }
 
-    for (int i = 0; i < f->size; ++i)
-        for (int j = 0; j < s->size; ++j)
-            res.coefficients[i + j] += f->coefficients[i] * s->coefficients[j];
+    for (int i = 0; i < a->size; ++i)
+        for (int j = 0; j < b->size; ++j)
+            res.coeff[i + j] += a->coeff[i] * b->coeff[j];
 
     return res;
 }
 
 int main()
 {
-    struct poly first, second, res;
+    struct poly a, b, res;
 
-    read_input(&first, &second);
+    read_input(&a, &b);
 
-    res = poly_mult(&first, &second);
+    res = poly_mult(&a, &b);
 
-    if (res.coefficients != NULL)
+    if (res.coeff != NULL)
     {
         int max_pow = 0;
         for (int i = 0; i < res.size; ++i)
-            if (res.coefficients[i] != 0)
+            if (res.coeff[i] != 0)
                 max_pow = i;
 
         for (int i = 0; i <= max_pow; ++i)
-            printf((i < max_pow) ? "%d " : "%d\n", res.coefficients[i]);
+            printf((i < max_pow) ? "%d " : "%d\n", res.coeff[i]);
     }
     
-    free(first.coefficients);
-    free(second.coefficients);
-    free(res.coefficients);
+    free(a.coeff);
+    free(b.coeff);
+    free(res.coeff);
 }
